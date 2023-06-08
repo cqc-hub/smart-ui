b8777708ce9df4be73cb74fdee4979bf6c8c4615
cqc <chenqinchuan@herenit.com>
2023-06-08 14:32:13 +0800

    feat: replace vuepress version

--- config/unocss.ts
+++ config/unocss.ts
@@ -13,6 +13,7 @@ const colors = [
   "purple",
   "pink",
 ];
+
 const safelist = [
   ...colors.map((v) => `bg-${v}-100`),
   ...colors.map((v) => `bg-${v}-400`),
@@ -25,7 +26,7 @@ const safelist = [
   ...colors.map((v) => `border-${v}-500`),
   ...colors.map((v) => `text-${v}-500`),
   ...colors.map((v) => `hover:text-${v}-500`),
-  'text-white',
+  "text-white",
   ...Array.from({ length: 8 }, (_, i) => `px-${i + 1}`),
   ...Array.from({ length: 8 }, (_, i) => `py-${i + 1}`),
   ...["xs", "sm", "base", "lg", "xl", "2xl", "3xl"].map((v) => `text-${v}`),
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,15 +1,15 @@
-import Theme from "vitepress/dist/client/theme-default";
 import SmartyUI from "../../../src/entry";
-import "vitepress-theme-demoblock/theme/styles/index.css";
+// import "vitepress-theme-demoblock/theme/styles/index.css";
+import DefaultTheme from "vitepress/theme";
 
+// 主题样式
+import "vitepress-theme-demoblock/dist/theme/styles/index.css";
 // 插件的组件，主要是demo组件
-// @ts-expect-error
-import Demo from "vitepress-theme-demoblock/components/Demo.vue";
-// @ts-expect-error
-import DemoBlock from "vitepress-theme-demoblock/components/DemoBlock.vue";
+import Demo from "vitepress-theme-demoblock/dist/client/components/Demo.vue";
+import DemoBlock from "vitepress-theme-demoblock/dist/client/components/DemoBlock.vue";
 
 export default {
-  ...Theme,
+  ...DefaultTheme,
   enhanceApp({ app }) {
     app.use(SmartyUI);
     app.component("Demo", Demo);
